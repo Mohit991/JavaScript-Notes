135254799338bf93145bdf94a7b9a49a0f9840b1
Mohit Sharma <mohit94596@gmail.com>
2024-10-23 14:14:19 +0530

    Basics added

---
+++ Variables/index.js
@@ -0,0 +1,14 @@
+console.log(`hello`);
+
+window.alert(`This is an alert!`)
+window.alert(`I Like Food`)
+
+//This is single line comment
+
+
+/* This is 
+multi line
+comment*/
+
+document.getElementById("myH1").textContent = "Mohit"
+document.getElementById("myP").textContent = "Sharma"
